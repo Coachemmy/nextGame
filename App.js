b4aba74ca7ca1447c1a68063f1b8cfb3a3dc4cbb
coachemmy <coachemmyb@gmail.com>
2023-02-03 11:36:54 +0100

    nextgame

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
+import { Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const colors = [
+'magenta','blue','teal','green','indigo','brown','olive','gray','red','orange','purple','black'
+];
+
+const App = () => {   // practice color recognition and memory skills.
+
+    const [selectedColor, setSelectedColor] = useState('');
+   
+    const handlePress = color => {
+        setSelectedColor(color)
+    }
+
+    const handleReset = () => {
+        setSelectedColor('')
+    }
+
+    return (
+        <View style = {styles.container}>
+            <Text style = {styles.title}>Memory Color Game</Text>
+            <View style = {styles.scoreContainer}>
+                <Text style = {styles.score}>Selected Color: {selectedColor}</Text>
+                <Button onPress = {handleReset} title = 'Reset'/>
+            </View>
+            <View style = {styles.buttonsContainer}>
+                {colors.map(color =>(
+                    <TouchableOpacity
+                        key={color}
+                        onPress = {() =>handlePress(color)}
+                        style = {[styles.button, {backgroundColor: color}]}
+                    >
+                        <Text style = {styles.lettering}> Guess My Color</Text>
+                     </TouchableOpacity>
+                ))}
+           </View> 
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 32,
+        marginBottom: 20
+    },
+    scoreContainer: {
+        marginBottom: 20
+    }, 
+    score:{
+        fontSize: 20,
+    }, 
+    buttonsContainer:{
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    button: {
+        width: 100,
+        height: 100,
+        margin: 10,
+        borderRadius: 20
+    },
+
+    lettering:{
+        fontSize: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+        marginLeft: 10,
+        marginTop: 40
+    }
+})
+
+export default App;
